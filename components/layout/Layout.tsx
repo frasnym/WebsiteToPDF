3facc45f6385706a691281ebcd9bde63b3666481
frasnym <frastyawan.nym@gmail.com>
2021-02-20 16:20:19 +0800

    feat(layout): Add footer to layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,41 +1,29 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Head from 'next/head';
+import Footer from './footer/Footer';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+	children?: ReactNode;
+	title?: string;
+};
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+	<main className="bg-purple-200">
+		<Head>
+			<title>{title} - FrasNym</title>
+			<meta
+				name="viewport"
+				content="initial-scale=1.0, width=device-width"
+			/>
+			<link rel="shortcut icon" href="/logo.svg" />
+		</Head>
+		<div className="w-full flex content-center items-center">
+			<div className="m-auto shadow-lg sm:rounded-3xl p-3 mb-3 sm:p-20 lg:w-4/5 bg-gray-100">
+				{children}
+			</div>
+		</div>
+		<Footer />
+	</main>
+);
 
-export default Layout
+export default Layout;
