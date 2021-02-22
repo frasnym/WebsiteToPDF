a44bf319cceb87690ab153f7395dbbee805d999b
frasnym <frastyawan.nym@gmail.com>
2021-02-22 12:20:48 +0800

    feat(converter-api): Implement chrome-aws-lambda

--- pages/api/converter.ts
+++ pages/api/converter.ts
@@ -1,7 +1,8 @@
 import { NextApiHandler } from 'next';
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
 
-import { isValidURL } from '../../helpers/utils';
+import { addhttp, isValidURL } from '../../helpers/utils';
 
 const converter: NextApiHandler = async (req, res) => {
 	const reqBody: { url: string } = req.body;
@@ -10,34 +11,50 @@ const converter: NextApiHandler = async (req, res) => {
 		return res.status(400).json({ message: 'Invalid URL' });
 	}
 
-	const browser = await puppeteer.launch({
-		headless: true,
-	});
+	try {
+		// const browser = await puppeteer.launch( { args: ['--no-sandbox'] } );
+		const browser = await chromium.puppeteer.launch({
+			args: [
+				...chromium.args,
+				'--hide-scrollbars',
+				'--disable-web-security',
+			],
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath,
+			headless: true,
+			ignoreHTTPSErrors: true,
+		});
+		// const browser = await puppeteer.launch({
+		// 	headless: true,
+		// });
 
-	const url = reqBody.url;
+		const url = addhttp(reqBody.url);
 
-	const page = await browser.newPage();
-	await page.setViewport({ width: 1200, height: 800 });
-	await page.goto(url, {
-		waitUntil: 'networkidle0',
-	});
+		const page = await browser.newPage();
+		await page.setViewport({ width: 1200, height: 800 });
+		await page.goto(url, {
+			waitUntil: 'networkidle0',
+		});
 
-	const pdf = await page.pdf({
-		// scale: 0.5,
-		// path: 'webtopdf/web.pdf',
-		format: 'letter',
-		margin: {
-			top: '20px',
-			bottom: '40px',
-			left: '20px',
-			right: '20px',
-		},
-	});
+		const pdf = await page.pdf({
+			// scale: 0.5,
+			// path: 'webtopdf/web.pdf',
+			format: 'letter',
+			margin: {
+				top: '20px',
+				bottom: '40px',
+				left: '20px',
+				right: '20px',
+			},
+		});
 
-	browser.close();
+		browser.close();
 
-	res.setHeader('Content-Type', 'application/pdf');
-	res.status(200).send(pdf);
+		res.setHeader('Content-Type', 'application/pdf');
+		return res.status(200).send(pdf);
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
 };
 
 export default converter;
